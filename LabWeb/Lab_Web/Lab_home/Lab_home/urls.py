abd71383818ef231eab2b016ba0f3760c11d7667
wysjmy <102455430+wysjmy@users.noreply.github.com>
2023-07-17 22:48:14 +0800

    Add files via upload

--- LabWeb/Lab_Web/Lab_home/Lab_home/urls.py
+++ LabWeb/Lab_Web/Lab_home/Lab_home/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from Web_home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index)
+    # path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('admin/',views.admin)
 ]
