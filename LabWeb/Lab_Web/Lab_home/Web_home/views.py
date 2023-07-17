a3931b41dd6e8492c0efa9e853849bb37074449c
wysjmy <102455430+wysjmy@users.noreply.github.com>
2023-07-17 22:48:14 +0800

    Add files via upload

--- LabWeb/Lab_Web/Lab_home/Web_home/views.py
+++ LabWeb/Lab_Web/Lab_home/Web_home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index1.html')
+
+def admin(request):
+    return render(request, 'admin.html')
--- Lab_Web/Lab_home/Web_home/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebHomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Web_home'
