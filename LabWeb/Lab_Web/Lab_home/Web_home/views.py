af3ef0981f16308e091e185b44b5ae037e8ae29f
lazyliang886 <134756188+lazyliang886@users.noreply.github.com>
2023-07-19 09:30:19 +0800

    change page

--- LabWeb/Lab_Web/Lab_home/Web_home/views.py
+++ LabWeb/Lab_Web/Lab_home/Web_home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index1.html')
+    return render(request, 'home.html')
 
 def admin(request):
     return render(request, 'admin.html')
+
+def research_dir(request):
+    return render(request, 'research.html')
+
+def team_t_s(request):
+    return render(request, 'team_t_s.html')
