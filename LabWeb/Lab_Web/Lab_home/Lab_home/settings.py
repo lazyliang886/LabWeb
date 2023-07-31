dfa64713f8e5af8dfd948e28b78530965a01e19a
lazyliang886 <2481752938@qq.com>
2023-07-31 14:02:30 +0800

    7.31afternoon

--- LabWeb/Lab_Web/Lab_home/Lab_home/settings.py
+++ LabWeb/Lab_Web/Lab_home/Lab_home/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 import os
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'Lab_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_project',
+        'USER': 'root',
+        'PASSWORD': '121573',
+        'HOST': 'localhost',
+        'POST': '3306',
     }
 }
 
